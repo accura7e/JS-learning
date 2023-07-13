ff939efc4961b7386e14698693b5ed517ac036be
accura7e <accura7ee@gmail.com>
2023-07-13 18:37:06 +0300

    add: added variables

---
+++ 01-base-start/app.js
@@ -0,0 +1,30 @@
+let num = 15
+let firstName = 'Philipp'
+const isProgrammer = true
+
+/* Can Do
+
+ let $num = 42
+
+ let _ = 49
+
+ let first_name = "Julia" //bad
+ let myNum = 34 // good
+ let num42 = 10
+*/
+
+/* Restricted
+
+ let 42num = 11
+ let my-num = 1
+ let const = 1
+*/
+
+
+// num = 30
+// isProgrammer = false // error because of const
+
+
+// alert(firstName)
+
+// console.log('Test:', firstName)
