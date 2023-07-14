415b17d230a79fe6d8f48132b029a719315344ef
accura7e <accura7ee@gmail.com>
2023-07-14 23:44:00 +0300

    add: added operators

--- 01-base-start/app.js
+++ 01-base-start/app.js
@@ -28,3 +28,17 @@ const isProgrammer = true
 // alert(firstName)
 
 // console.log('Test:', firstName)
+
+// console.log(num +  10)
+// console.log(num -  10)
+// console.log(num *  10)
+// console.log(num /  10)
+// console.log(num)
+
+// let num2 = num + 10
+// console.log(num, num2)
+// num = num2 - num
+// console.log(num, num2)
+
+let num3 = (num + 10) * 2 / 5 - 1
+console.log(num3)
