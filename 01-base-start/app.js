449c1ff642c05e72a0f45bf374d6f30f04dc2bed
accura7e <accura7ee@gmail.com>
2023-07-23 20:44:09 +0300

    optimize: optimized code

--- 01-base-start/app.js
+++ 01-base-start/app.js
@@ -67,19 +67,31 @@ minusBtn.onclick = function(){
     action = '-'
 
 }
+function printResult(result){
+        if(result < 0){
+            resultElement.style.color = "red"
+        } else{
+            resultElement.style.color = "green"
+        }
+        resultElement.textContent = result
+}
+
+function computeNumbersWithAction(inp1, inp2, actionSymbol){
+    
+    const num1 = Number(inp1.value)
+    const num2 = Number(inp2.value)
+
+    return actionSymbol == '+' ? num1 + num2 : num1 - num2
+}
 
 submitBtn.onclick = function (){
+    const result = computeNumbersWithAction(input1, input2, action)
+    printResult(result)
 
-    if (action == '+') {
-        const sum = Number(input1.value) + Number(input2.value)
-    resultElement.textContent = sum
-    }
-
-    if (action == '-') {
-        const sum = Number(input1.value) - Number(input2.value)
-    resultElement.textContent = sum
-    }
     
+   
+
+
     
 }
 
