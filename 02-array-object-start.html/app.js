7fe01beeed063dae83c553cc88533ba0343809c2
accura7e <accura7ee@gmail.com>
2023-07-31 00:41:42 +0300

    add: added array

---
+++ 02-array-object-start.html/app.js
@@ -0,0 +1,14 @@
+const array = [1, 2, 3, 4, 5, 6, 7]
+// const arrayList = ['a', 'b']
+
+// const array = new Array(1,2,3,4)
+
+// console.log(array.length)
+
+console.log(array[0])
+
+console.log(array[array.length- 1]) // array[6 - 1 = 5]
+array[0] = 'Privet!'
+console.log(array)
+array[array.length] = 'becon'
+console.log(array)
