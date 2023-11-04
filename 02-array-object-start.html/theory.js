29ffd1743d0cb25b750057e73d2d108fb2e5a7ef
accura7e <accura7ee@gmail.com>
2023-11-04 22:11:37 +0300

    add: added new methods

---
+++ 02-array-object-start.html/theory.js
@@ -0,0 +1,32 @@
+const names = ['Филипп', 'Максим', 'Леха', 'Тимур']
+
+
+// names.push('Кирилл')
+// names.unshift('Кирилл')
+// names.shift()
+//  names.pop()
+// console.log('names: ', names)
+
+// console.log(names.reverse())
+
+// console.log(names.toReversed())
+// console.log(names)
+
+// console.log(names.toSorted())
+// console.log(names)
+
+// const index = names.indexOf('Максим')
+// console.log(index)
+
+// const newNames = names.with(3, '')
+
+// names[index] = ''
+// const capitalNames = names.map(function(name, index){
+//     if (index === 1){
+//         return name.toUpperCase()
+//     }
+//     return name
+// })
+
+console.log(names.includes('Филипп'))
+
